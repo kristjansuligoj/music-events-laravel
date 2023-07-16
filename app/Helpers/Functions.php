e0f3cb5202db80f2a16c4c9e24c8739890064174
kristjansuligoj <kristjan.suligoj@aspiria.com>
2023-07-16 20:32:44 +0200

    Add function to change genre to index

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -3,9 +3,40 @@
     function printArray($data, $field): string {
         $html = "";
 
-        foreach($data->$field as $field) {
-            $html .= "<li>" . $field . "</li>";
+        foreach($data as $field) {
+            $html .= "<li>" . $field->name . "</li>";
         }
 
         return $html;
+    }
+
+    function genreToIndex($genres): array {
+        $genreIds = [];
+        foreach($genres as $genre) {
+            switch ($genre) {
+                case "Rock":
+                    $genreIds[] = 1;
+                    break;
+                case "Metal":
+                    $genreIds[] = 2;
+                    break;
+                case "Rap":
+                    $genreIds[] = 3;
+                    break;
+                case "Country":
+                    $genreIds[] = 4;
+                    break;
+                case "Hip hop":
+                    $genreIds[] = 5;
+                    break;
+                case "Jazz":
+                    $genreIds[] = 6;
+                    break;
+                case "Electronic":
+                    $genreIds[] = 7;
+                    break;
+            }
+        }
+
+        return $genreIds;
     }
