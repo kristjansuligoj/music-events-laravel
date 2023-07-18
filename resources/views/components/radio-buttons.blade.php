68a34db0119c1502e9a4a25745b45e01ed0e3248
kristjansuligoj <kristjan.suligoj@aspiria.com>
2023-07-18 20:20:41 +0200

    Add component for radio buttons

--- resources/views/components/radio-buttons.blade.php
+++ resources/views/components/radio-buttons.blade.php
@@ -1,6 +1,9 @@
 @props(['data'])
 
-@foreach($data as $entry)
-    <input type="radio" name="musician" value="{{ $entry->id }}">
+@foreach($data['musicians'] as $entry)
+    <input type="radio" name="musician" value="{{ $entry->id }}"
+        @if($entry->id == $data['selectedMusician']) checked @endif
+    >
+
     <label for="">{{ $entry->name }}</label><br>
 @endforeach <hr>
