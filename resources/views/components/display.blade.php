965d7c6c2ba88a4ad386cbf106469bd1eefb90fd
kristjansuligoj <kristjan.suligoj@aspiria.com>
2023-07-22 20:51:40 +0200

    Delete display.blade.php

--- resources/views/components/display.blade.php
+++
@@ -1,9 +0,0 @@
-<a href="/">Back</a><br>
-@props(['component', 'data'])
-
-<div>
-    <h3>{{ $component }}s page</h3>
-    <article>
-        @component($component.'s.'.$component, [$component => $data]) @endcomponent
-    </article>
-</div>
