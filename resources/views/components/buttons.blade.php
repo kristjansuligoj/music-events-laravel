320648ba01f7fea3ca63b98c87460b90c3ab8295
kristjansuligoj <kristjan.suligoj@aspiria.com>
2023-07-22 20:48:58 +0200

    Helping components reworked with bootstrap

--- resources/views/components/buttons.blade.php
+++ resources/views/components/buttons.blade.php
@@ -1,13 +1,21 @@
 @props(['data'])
 
-<div>
+<div class="d-flex justify-content-around m-5">
+    <a
+        class="btn btn-dark mb-3"
+        style="width:200px"
+        href="/{{ $data['name'] }}s/edit/{{ $data['id'] }}"
+    >Edit {{ $data['name'] }}</a>
+
     <form method="post" action="/{{ $data['name'] }}s/remove/{{ $data['id'] }}">
         @csrf
         @method('DELETE')
 
-        <input type="submit" value="Remove {{ $data['name'] }}">
+        <input
+            class="btn btn-danger"
+            style="width:200px"
+            type="submit"
+            value="Remove {{ $data['name'] }}"
+        >
     </form>
-
-    <a href="/{{ $data['name'] }}s/edit/{{ $data['id'] }}">Edit {{ $data['name'] }}</a>
-    <hr>
 </div>
--- resources/views/components/checkboxes.blade.php
+++ resources/views/components/checkboxes.blade.php
@@ -3,7 +3,9 @@
 <div>
     <label for="{{$data['name']}}">{{ucfirst($data['name'])}}: </label><br>
     @foreach($data['options'] as $option)
-        <input type="checkbox" id="{{$data['name']}}{{$loop->iteration}}" name="{{$data['name']}}[]" value="{{$option}}"
+        <input
+            class="m-2"
+            type="checkbox" id="{{$data['name']}}{{$loop->iteration}}" name="{{$data['name']}}[]" value="{{$option}}"
            @if(isset($data['selectedData']) && $data['selectedData']->contains('name', $option))
                checked
            @endif
