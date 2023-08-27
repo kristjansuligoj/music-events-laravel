1844efe37a8c30f34b6d3a99a977f6cb82f1ddaf
kristjansuligoj <kristjan.suligoj@aspiria.com>
2023-08-27 08:14:05 +0200

    Fixed seeders and factories so images are displayed properly

---
+++ database/seeders/GenreSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\GenresEnum;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GenreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $genres = GenresEnum::getAllGenres();
+
+        foreach($genres as $genre) {
+            DB::table('genres')->insert([
+                'name' => $genre
+            ]);
+        }
+    }
+}
