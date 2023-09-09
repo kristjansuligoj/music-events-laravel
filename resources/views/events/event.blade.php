4123eedc7ae192f9b4a160c986ee7dbfb7ca9385
kristjansuligoj <kristjan.suligoj@aspiria.com>
2023-09-09 13:16:14 +0200

    Everyone(even users that are not logged in) can see participants of an event

--- resources/views/events/event.blade.php
+++ resources/views/events/event.blade.php
@@ -24,7 +24,34 @@
             <b>Ticket price:</b> {{ $event->ticketPrice }}€<br>
 
             <b>Musician:</b> {{$event->musicians[0]->name}}<br>
+
+            @if(isset($event->participants[0]))
+                <b>Users that are going to this event: </b><br>
+                <ul>
+                    @foreach($event->participants as $participant)
+                        <li>
+                            {{ $participant->name }}
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <b>No users are attending this event.</b>
+            @endif
         </div>
+
+        @if(Auth::id())
+            @if($event->participants->contains('id', Auth::id()))
+                <form action="/events/{{$event->id}}/remove-user/{{Auth::id()}}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-success w-auto m-3">NOT GOING</button>
+                </form><br>
+            @else
+                <form action="/events/{{$event->id}}/add-user/{{Auth::id()}}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-success w-auto m-3">GOING</button>
+                </form><br>
+            @endif
+        @endif
     </div>
 @endsection
 
