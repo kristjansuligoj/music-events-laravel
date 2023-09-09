148554b8a85a37fe616f680ebf16a8537feee44e
kristjansuligoj <kristjan.suligoj@aspiria.com>
2023-09-09 13:14:54 +0200

    Created user migration, so users can now register and login

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,6 +7,27 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
     </head>
     <body>
+        <div class="p-2">
+            @if(Auth::check())
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-success w-auto m-3">Logout</button>
+                </form><br>
+
+                <h2>Logged in user: {{Auth::user()->name}}
+            @else
+                <a
+                    href="{{ route('register') }}"
+                    class="btn btn-success w-auto m-3"
+                >Register</a>
+
+                <a
+                    href="{{ route('login') }}"
+                    class="btn btn-success w-auto m-3"
+                >Login</a>
+            @endif
+        </div>
+
         <div class="d-flex justify-content-between p-4">
             <a
                 href="{{ route('musicians.list') }}"
