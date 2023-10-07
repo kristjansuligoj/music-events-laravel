4cb9bc6b948722e30e5c3d57df98fdb7a9ae4f62
kristjansuligoj <kristjan.suligoj@aspiria.com>
2023-10-07 21:01:46 +0200

    User model changed, so email can be sent to them

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use HasUuids;
+    use Notifiable;
 
     protected $fillable = [
         'name',
