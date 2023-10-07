fb85824518cb16c04072799f1b6f0cffa7f88f1d
kristjansuligoj <kristjan.suligoj@aspiria.com>
2023-10-07 21:01:31 +0200

    User can now press on "Forgot password?" to reset password

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/forgot-password', function () {
+    return view('forgot-password');
+})->name('forgot-password');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
