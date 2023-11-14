b2e46e304ba465498d8e165ca65f260b07efbc5e
kristjansuligoj <kristjan.suligoj@aspiria.com>
2023-11-14 19:28:45 +0100

    Added search value into search field after searching

--- resources/views/components/searchbar.blade.php
+++ resources/views/components/searchbar.blade.php
@@ -4,10 +4,12 @@
         placeholder="Search by keyword . . ."
         name="keyword"
         class="form-control border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 mb-2"
+        value="{{ request()->query('keyword') }}"
     >
     <input type="submit" value="Search" class="inline-flex items-center px-4 py-2 bg-gray-800 border
         border-transparent rounded-md text-xs text-white
         tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900
         focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
-        transition ease-in-out duration-150 ml-4 text-decoration-none uppercase">
+        transition ease-in-out duration-150 ml-4 text-decoration-none uppercase"
+    >
 </form>
