41c316267aa2b361aec5ca1d1a5a3d12ca21ddb3
kristjansuligoj <kristjan.suligoj@aspiria.com>
2023-11-18 09:52:02 +0100

    Musician view now also displays which songs they created and which events they're performing at

--- resources/views/musicians/musician.blade.php
+++ resources/views/musicians/musician.blade.php
@@ -22,6 +22,26 @@
 
             <b>Genre:</b><br>
             <?php echo printArray($musician->genres, "name"); ?>
+
+            <hr>
+
+            @php
+                $data['elements'] = $musician->songs;
+                $data['property'] = "title";
+                $data['text'] = $musician->name . " has written the following song";
+            @endphp
+            <div class="mb-3">
+                <x-relations-list :data="$data"/>
+            </div>
+
+            @php
+                $data['elements'] = $musician->events;
+                $data['property'] = "name";
+                $data['text'] = $musician->name . " is performing in the following event";
+            @endphp
+            <div class="mb-3">
+                <x-relations-list :data="$data"/>
+            </div>
         </div>
     </div>
 @endsection
