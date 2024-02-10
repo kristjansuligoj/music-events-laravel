5f4504e0730bc777ba0464c05e7569418c27c6f2
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-02-10 19:22:09 +0100

    Event components updated, to work with all features

--- resources/frontend/src/app/components/events/event-list/event-list.component.ts
+++ resources/frontend/src/app/components/events/event-list/event-list.component.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ButtonComponent} from "../../shared/button/button.component";
+import {MusicianPreviewComponent} from "../../musicians/musician-preview/musician-preview.component";
+import {NgForOf, NgIf} from "@angular/common";
+import {SearchBarComponent} from "../../shared/search-bar/search-bar.component";
+import {EventService} from "../../../services/event.service";
+import {AuthService} from "../../../services/auth.service";
+import {EventPreviewComponent} from "../event-preview/event-preview.component";
 
 @Component({
   selector: 'app-event-list',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonComponent,
+    MusicianPreviewComponent,
+    NgForOf,
+    NgIf,
+    SearchBarComponent,
+    EventPreviewComponent
+  ],
+  providers: [
+    EventService
+  ],
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.css'
 })
-export class EventListComponent {
+export class EventListComponent implements OnInit {
+  events: any[] = [];
+  nextPageUrl: string | null = null;
+  prevPageUrl: string | null = null;
 
+  constructor(
+    public eventService: EventService,
+    public authService: AuthService,
+  ) {}
+  public ngOnInit() {
+    this.eventService.allEvents().subscribe({
+      next: (response: any) => {
+        this.events = response.data.events.data;
+        this.nextPageUrl = response.data.events.next_page_url;
+        this.prevPageUrl = response.data.events.prev_page_url;
+      },
+      error: (error) => {
+        console.error('Error fetching events:', error);
+      }
+    });
+  }
+
+  goToNextPage(): void {
+    if (this.nextPageUrl) {
+      this.eventService.paginatedEvents(this.nextPageUrl).subscribe({
+        next: (response: any) => {
+          this.events = response.data.events.data;
+          this.nextPageUrl = response.data.events.next_page_url;
+          this.prevPageUrl = response.data.events.prev_page_url;
+        },
+        error: (error) => {
+          console.error('Error fetching events:', error);
+        }
+      });
+    }
+  }
+
+  goToPrevPage(): void {
+    if (this.prevPageUrl) {
+      this.eventService.paginatedEvents(this.prevPageUrl).subscribe({
+        next: (response: any) => {
+          this.events = response.data.events.data;
+          this.nextPageUrl = response.data.events.next_page_url;
+          this.prevPageUrl = response.data.events.prev_page_url;
+        },
+        error: (error) => {
+          console.error('Error fetching events:', error);
+        }
+      });
+    }
+  }
 }
