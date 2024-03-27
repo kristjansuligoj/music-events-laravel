c9870b224ab54aa3ad2ce0b8d6393b35e645733d
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-03-27 16:53:35 +0100

    Created number-card-chart component, to display number card charts

---
+++ resources/frontend/src/app/components/shared/number-card-chart/number-card-chart.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input} from '@angular/core';
+import {NgxChartsModule} from "@swimlane/ngx-charts";
+
+@Component({
+  selector: 'app-number-card-chart',
+  standalone: true,
+  imports: [
+    NgxChartsModule
+  ],
+  templateUrl: './number-card-chart.component.html',
+})
+export class NumberCardChartComponent {
+  @Input() public data: any[] = [];
+  @Input() public size: [number, number] = [700, 400];
+  @Input() public cardColor: string = '#232837';
+  @Input() public colorScheme: { domain } = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+}
