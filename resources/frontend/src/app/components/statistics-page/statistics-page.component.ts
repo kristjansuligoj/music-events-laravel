914d734966615c4f87ed94a0bc12e52f200b1469
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-03-27 22:52:53 +0100

    Added pie chart that shows which genre the logged user listens to the most

--- resources/frontend/src/app/components/statistics-page/statistics-page.component.ts
+++ resources/frontend/src/app/components/statistics-page/statistics-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PieChartComponent} from "../shared/pie-chart/pie-chart.component";
 import {VerticalBarChartComponent} from "../shared/vertical-bar-chart/vertical-bar-chart.component";
 import {NumberCardChartComponent} from "../shared/number-card-chart/number-card-chart.component";
@@ -6,6 +6,9 @@ import {ButtonComponent} from "../shared/button/button.component";
 import {NgIf} from "@angular/common";
 import {AuthService} from "../../services/auth.service";
 import {HrComponent} from "../shared/hr/hr.component";
+import {SongService} from "../../services/song.service";
+import {MusicianService} from "../../services/musician.service";
+import {EventService} from "../../services/event.service";
 
 @Component({
   selector: 'app-statistics-page',
@@ -18,13 +21,87 @@ import {HrComponent} from "../shared/hr/hr.component";
     NgIf,
     HrComponent
   ],
+  providers: [
+    MusicianService,
+    SongService,
+    EventService,
+  ],
   templateUrl: './statistics-page.component.html',
 })
-export class StatisticsPageComponent {
+export class StatisticsPageComponent implements OnInit {
+  public myMusicians: any = [];
+  public mySongs: any = [];
+  public myEvents: any = [];
+  public genreCount: any[] = [];
 
   public constructor(
     public authService: AuthService,
-  ) {
+    public musicianService: MusicianService,
+    public songService: SongService,
+    public eventService: EventService,
+  ) {}
 
+  public ngOnInit(): void {
+    const loggedUser: any = this.authService.getLoggedUser();
+
+    if (loggedUser) {
+      this.fetchMyAddedElements(loggedUser.id);
+    }
+  }
+
+  /**
+   * Fetches all elements the logged user has added, for further analysis
+   *
+   * @param { string } id
+   */
+  public fetchMyAddedElements(id: string): void {
+    this.musicianService.allMusicians(id, null, true).subscribe({
+      next: (response: any) => {
+        this.myMusicians = response.data.musicians;
+
+        this.songService.allSongs(id, null, true).subscribe({
+            next: (response: any) => {
+              this.mySongs = response.data.songs;
+
+              this.getGenreCount();
+
+              this.eventService.allEvents(id, null, true).subscribe({
+                  next: (response: any) => {
+                    this.myEvents = response.data.events;
+                  },
+                  error: (response: any) => {
+                    console.log(response);
+                  }
+                }
+              );
+            },
+            error: (response: any) => {
+              console.log(response);
+            }
+          }
+        );
+      },
+      error: (response: any) => {
+        console.log(response);
+      }
+    });
+  }
+
+  /**
+   * Goes through songs and counts how many times a genre appears,
+   * it then formats the data for pie chart representation
+   */
+  public getGenreCount(): void {
+    const genreCounts = this.mySongs.reduce((counts: any, song: any) => {
+      song.genres.forEach((genre: any) => {
+        counts[genre.name] = (counts[genre.name] || 0) + 1;
+      });
+      return counts;
+    }, {});
+
+    this.genreCount = Object.keys(genreCounts).map((name: string): any => ({
+      name,
+      value: genreCounts[name]
+    }));
   }
 }
