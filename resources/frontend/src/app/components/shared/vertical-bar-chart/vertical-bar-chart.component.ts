7943d3c14b85a9be1d06c4936530eaf741d3c986
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-03-27 16:53:21 +0100

    Created vertical-bar-chart component, to display vertical-bar-charts

---
+++ resources/frontend/src/app/components/shared/vertical-bar-chart/vertical-bar-chart.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input} from '@angular/core';
+import {NgxChartsModule} from "@swimlane/ngx-charts";
+
+@Component({
+  selector: 'app-vertical-bar-chart',
+  standalone: true,
+  imports: [
+    NgxChartsModule
+  ],
+  templateUrl: './vertical-bar-chart.component.html',
+})
+export class VerticalBarChartComponent {
+  @Input() public data: any[] = [];
+  @Input() public size: [number, number] = [400, 400];
+  @Input() public showXAxis: boolean = true;
+  @Input() public showYAxis: boolean = true;
+  @Input() public gradient: boolean = false;
+  @Input() public showLegend: boolean = true;
+  @Input() public showXAxisLabel: boolean = true;
+  @Input() public xAxisLabel: string = 'X axis label';
+  @Input() public showYAxisLabel: boolean = true;
+  @Input() public yAxisLabel: string = 'Y axis label';
+  @Input() public colorScheme: { domain } = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+}
