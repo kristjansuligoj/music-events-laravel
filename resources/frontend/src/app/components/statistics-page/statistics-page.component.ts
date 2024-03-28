6911ae241b1930fd30ce3904e89ce6bd2ba2d435
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-03-28 13:35:17 +0100

    Statistic page now gets all elements of user from the database

--- resources/frontend/src/app/components/statistics-page/statistics-page.component.ts
+++ resources/frontend/src/app/components/statistics-page/statistics-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PieChartComponent} from "../shared/pie-chart/pie-chart.component";
 import {VerticalBarChartComponent} from "../shared/vertical-bar-chart/vertical-bar-chart.component";
 import {NumberCardChartComponent} from "../shared/number-card-chart/number-card-chart.component";
@@ -6,6 +6,9 @@ import {ButtonComponent} from "../shared/button/button.component";
 import {NgIf} from "@angular/common";
 import {AuthService} from "../../services/auth.service";
 import {HrComponent} from "../shared/hr/hr.component";
+import {SongService} from "../../services/song.service";
+import {MusicianService} from "../../services/musician.service";
+import {EventService} from "../../services/event.service";
 
 @Component({
   selector: 'app-statistics-page',
@@ -18,13 +21,66 @@ import {HrComponent} from "../shared/hr/hr.component";
     NgIf,
     HrComponent
   ],
+  providers: [
+    MusicianService,
+    SongService,
+    EventService,
+  ],
   templateUrl: './statistics-page.component.html',
 })
-export class StatisticsPageComponent {
+export class StatisticsPageComponent implements OnInit {
+  public myMusicians: any = [];
+  public mySongs: any = [];
+  public myEvents: any = [];
 
   public constructor(
     public authService: AuthService,
-  ) {
+    public musicianService: MusicianService,
+    public songService: SongService,
+    public eventService: EventService,
+  ) {}
 
+  public ngOnInit(): void {
+    const loggedUser: any = this.authService.getLoggedUser();
+
+    if (loggedUser) {
+      this.fetchMyAddedElements(loggedUser.id);
+    }
+  }
+
+  /**
+   * Fetches all elements the logged user has added, for further analysis
+   *
+   * @param { string } id
+   */
+  public fetchMyAddedElements(id: string): void {
+    this.musicianService.allMusicians(id, null, true).subscribe({
+      next: (response: any) => {
+        this.myMusicians = response.data.musicians;
+
+        this.songService.allSongs(id, null, true).subscribe({
+            next: (response: any) => {
+              this.mySongs = response.data.songs;
+
+              this.eventService.allEvents(id, null, true).subscribe({
+                  next: (response: any) => {
+                    this.myEvents = response.data.events;
+                  },
+                  error: (response: any) => {
+                    console.log(response);
+                  }
+                }
+              );
+            },
+            error: (response: any) => {
+              console.log(response);
+            }
+          }
+        );
+      },
+      error: (response: any) => {
+        console.log(response);
+      }
+    });
   }
 }
