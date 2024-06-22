7f002c7f6d04c8d78daa257de5f0c0593f2bc5e0
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-06-22 08:48:29 +0200

    Updated project to laravel 11.11.0, php 8.3, and added changes needed to keep the project working

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,4 +28,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->belongsToMany(Event::class, 'event_participants', 'user_id', 'event_id');
     }
+
+    public function getAuthPasswordName()
+    {
+        return 'password';
+    }
 }
